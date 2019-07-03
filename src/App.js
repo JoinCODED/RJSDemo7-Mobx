6176ed6c285c0471449b316ac96ee30919aa65e8
Lailz <lailaabdulraheem@gmail.com>
2019-07-03 18:37:05 +0300

    update react

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 
 import Component1 from "./Component1";
 import Component2 from "./Component2";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="row">
-          <Component1 />
-          <Component2 />
-        </div>
+function App() {
+  return (
+    <div className="App">
+      <div className="row">
+        <Component1 />
+        <Component2 />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
